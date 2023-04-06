286ff931e7a9532678b25816a6b66d95de6bb05e
Botlhale Setou <botlhale.setou@gmail.com>
2023-04-06 22:01:08 +0200

    Complete functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   # A method that returns the 3 most recent posts for a given user.
   def most_recent_post
